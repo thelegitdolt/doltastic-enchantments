c84b13f472b8f062378f804ddccb5a55568c2b01
thelegitdolt <asphaltpotatofy@gmail.com>
2026-04-12 02:51:13 -0700

    books tooltips now render properly with more than 1 enchants

---
+++ src/main/java/com/dolthhaven/doltasticenchantments/core/mixin/AncientBookMixin.java
@@ -0,0 +1,45 @@
+package com.dolthhaven.doltasticenchantments.core.mixin;
+
+import com.dolthhaven.doltasticenchantments.core.ItemsUtil;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import me.alfie.immersiveenchanting.item.AncientBook;
+import me.alfie.immersiveenchanting.util.EnchantmentUtil;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.Holder;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+import java.util.List;
+
+@Mixin(AncientBook.class)
+public class AncientBookMixin {
+    @WrapOperation(method = "appendHoverText", slice = @Slice(
+            from = @At(value = "INVOKE", target = "Lnet/minecraft/core/Holder;value()Ljava/lang/Object;"),
+            to = @At(value = "INVOKE", target = "Lme/alfie/immersiveenchanting/datacomponent/ReplicatedNBT;isReplicated(Lnet/minecraft/world/item/ItemStack;)Z")
+    ), at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"))
+    private <E> boolean DoltasticEnchantments$RenderEveryEnchantment(List<Component> instance, E e, Operation<Boolean> original, @Local(argsOnly = true) ItemStack book, @Local RegistryAccess access) {
+        List<ResourceKey<Enchantment>> enchants = ItemsUtil.getAllStoredEnchantments(book);
+        if (enchants.size() == 1) return original.call(instance, e);
+        MutableComponent loreText = Component.translatable("lore.immersiveenchanting.ancient_book");
+        loreText.append(":");
+        instance.add(loreText);
+        for (ResourceKey<Enchantment> enchantKey : enchants) {
+            Holder<Enchantment> holder = EnchantmentUtil.getEnchantmentHolder(access, enchantKey).orElse(null);
+            if (holder == null) continue;
+
+            Enchantment enchantment = holder.value();
+            String name = enchantment.getDescriptionId();
+            instance.add(Component.literal("- ").append(Component.translatable(name)).withStyle(ChatFormatting.GOLD));
+        }
+        return true;
+    }
+}
