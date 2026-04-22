1ebbff05c3458a7019db58d818a77ec2bcfa393d
thelegitdolt <asphaltpotatofy@gmail.com>
2026-04-22 11:33:14 -0700

    tee hee

--- src/main/java/com/dolthhaven/doltasticenchantments/core/data/server/tags/DERecipes.java
+++ src/main/java/com/dolthhaven/doltasticenchantments/core/data/server/tags/DERecipes.java
@@ -22,9 +22,9 @@ public class DERecipes extends BlueprintRecipeProvider {
     public void buildRecipes(Consumer<FinishedRecipe> consumer) {
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, DEItems.FAIRY_DUST.get(), 3)
                 .requires(Ingredient.of(Items.DIAMOND, Items.POPPED_CHORUS_FRUIT))
-                .requires(Ingredient.of(Items.COPPER_INGOT, Items.AMETHYST_SHARD, Items.GOLD_INGOT, Items.GLOWSTONE_DUST))
+                .requires(Ingredient.of(Items.COPPER_INGOT, Items.AMETHYST_SHARD, Items.GOLD_INGOT, Items.GLOWSTONE_DUST, Items.HONEYCOMB))
                 .requires(Ingredient.of(Items.GLOW_INK_SAC, Items.PRISMARINE_CRYSTALS, Items.SCUTE, Items.NAUTILUS_SHELL))
-                .requires(Ingredient.of(Items.BLAZE_POWDER, Items.ENDER_PEARL, Items.REDSTONE, Items.CHARCOAL, Items.COAL, Items.HONEYCOMB))
+                .requires(Ingredient.of(Items.BLAZE_POWDER, Items.MAGMA_CREAM, Items.ENDER_PEARL, Items.REDSTONE, Items.CHARCOAL, Items.COAL))
                 .unlockedBy("has_lapis", has(Items.LAPIS_LAZULI)).save(consumer);
         SpecialRecipeBuilder.special(DERecipeSerializers.ANCIENT_BOOK.get()).save(consumer, "%s:ancient_book_from_diamonds".formatted(DoltasticEnchantments.MOD_ID));
     }
