f1ce3c1bd58ee27c4caa0e46646a57df96c342b7
thelegitdolt <asphaltpotatofy@gmail.com>
2026-04-22 02:09:51 -0700

    Hide reliably removed enchantments from creative tab

---
+++ src/main/java/com/dolthhaven/doltasticenchantments/core/mixin/CreativeEventsMixin.java
@@ -0,0 +1,20 @@
+package com.dolthhaven.doltasticenchantments.core.mixin;
+
+import com.dolthhaven.doltasticenchantments.core.DoltasticEnchantments;
+import com.dolthhaven.doltasticenchantments.integration.emi.DEReliableRemoverCompat;
+import me.alfie.immersiveenchanting.events.creative.CreativeEvents;
+import net.minecraft.core.Holder;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CreativeEvents.class)
+public class CreativeEventsMixin {
+    @Inject(method = "lambda$onBuildModCreativeTab$0", at = @At("HEAD"), cancellable = true)
+    private static void DoltasticEnchantments$HideReliablyRemovedBooks(BuildCreativeModeTabContentsEvent event, Holder.Reference<Enchantment> holder, CallbackInfo ci) {
+        if (DoltasticEnchantments.reliableRemover() && DEReliableRemoverCompat.isEnchantmentRemoved(holder.value())) ci.cancel();
+    }
+}
